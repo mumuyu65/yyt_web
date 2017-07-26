c034f5fe214aa3572132b23adfebf9a1611e5595
chenlong <chenlongwill@qq.com>
2017-07-26 17:28:21 +0800

    产品介绍部分功能完成

--- src/pages/productsintro.vue
+++ src/pages/productsintro.vue
@@ -1,3 +1,34 @@
 <template>
- <p>this is productsIntro page!</p>
+    <img v-bind:src='products' style="width:100%;" />
 </template>
+
+<script>
+import API from '@/api/API'
+//实例化api
+const api = new API();
+export default {
+  name: 'News',
+  data () {
+    return {
+        products:'',
+    }
+  },
+  mounted (){
+    this.initData();
+  },
+  methods:{
+    initData (){
+        let that = this;
+
+        api.productIntro().then(function(res){
+            if(res.data.Code ==3){
+                that.products=res.data.Data[0].img_url;
+            }
+        }).catch(function(err){
+          console.log(err);
+        });
+    },
+  }
+}
+
+</script>
