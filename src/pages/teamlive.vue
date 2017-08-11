a6a5888d118501f61810ccc5b00b27aa85858bba
chenlong <chenlongwill@qq.com>
2017-08-11 17:42:48 +0800

    战队介绍功能完成

--- src/pages/teamlive.vue
+++ src/pages/teamlive.vue
@@ -1,3 +1,35 @@
 <template>
-    <h1>战队直播介绍</h1>
+   <img v-bind:src="clanIntro"  style="width:100%; height:100%;"/>
 </template>
+
+<script>
+import API from '@/api/API'
+//实例化api
+const api = new API();
+
+export default{
+    data(){
+        return{
+            clanIntro:'',
+        }
+    },
+
+    mounted(){
+        this.initData();
+    },
+    methods:{
+      initData(){
+        let that = this;
+        api.clanIntro().then(function(res){
+            if(res.data.Code ==3){
+                that.clanIntro = res.data.Data[0].img_url;
+            }else{
+                alert(res.data.Msg);
+            }
+        }).catch(function(err){
+            console.log(err);
+        });
+      }
+    }
+}
+</script>
